c9818afdea3383db843bd69c35a19c1b459a9175
plaidbait91 <f20200055@hyderabad.bits-pilani.ac.in>
2022-02-13 00:42:09 +0530

    Add main menu and competitor screens with navigate to home option.

--- android app/app/src/main/java/com/example/farmerscollective/MainActivity.kt
+++ android app/app/src/main/java/com/example/farmerscollective/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.farmerscollective
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.Navigation
 import com.example.farmerscollective.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var controller: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        controller = Navigation.findNavController(this, R.id.nav_host_fragment_container)
+    }
+
+    fun toHome(view: View) {
+        controller.navigate(R.id.mainFragment)
     }
 }
