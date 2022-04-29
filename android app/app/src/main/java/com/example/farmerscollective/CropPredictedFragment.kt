8926356e022912a98042da698ea65beb833a840d
plaidbait91 <f20200055@hyderabad.bits-pilani.ac.in>
2022-04-29 13:30:29 +0530

    Connect to Cloud Firestore

--- android app/app/src/main/java/com/example/farmerscollective/CropPredictedFragment.kt
+++ android app/app/src/main/java/com/example/farmerscollective/CropPredictedFragment.kt
@@ -3,12 +3,16 @@ package com.example.farmerscollective
 import android.graphics.Color
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
@@ -18,8 +22,12 @@ import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.ChartTouchListener
+import com.github.mikephil.charting.listener.OnChartGestureListener
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 
-class CropPredictedFragment : Fragment() {
+class CropPredictedFragment : Fragment(), OnChartGestureListener {
 
     companion object {
         fun newInstance() = CropPredictedFragment()
@@ -30,6 +38,7 @@ class CropPredictedFragment : Fragment() {
     private lateinit var text: TextView
     private lateinit var icon: ImageView
     private lateinit var chart: LineChart
+    private lateinit var dates: ArrayList<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,6 +49,10 @@ class CropPredictedFragment : Fragment() {
             "corn" to R.drawable.corn, "jute" to R.drawable.jute)
 
         viewModel = ViewModelProvider(this)[CropPredictedViewModel::class.java]
+        val data = ArrayList<ILineDataSet>()
+        val values1 = ArrayList<Entry>()
+        dates = ArrayList()
+        val prices = ArrayList<Float>()
 
         text = view.findViewById(R.id.crop_predict)
         icon = view.findViewById(R.id.crop_icon)
@@ -49,39 +62,6 @@ class CropPredictedFragment : Fragment() {
         map.forEach {
             if(it.first == args.crop) icon.setImageResource(it.second)
         }
-
-        val data = ArrayList<ILineDataSet>()
-        val values1 = ArrayList<Entry>()
-        val values2 = ArrayList<Entry>()
-
-        val csv = activity!!.assets.open("RAJASTHAN_KOTA_Price.csv")
-        val dates = ArrayList<String>()
-        val prices = ArrayList<Float>()
-
-        csvReader().open(csv) {
-            readAllAsSequence().forEach { line ->
-                if(line[0] != "DATE" && line[1].isNotEmpty()) {
-                    dates.add(line[0])
-                    prices.add(line[1].toFloat())
-                }
-            }
-        }
-
-        val len = dates.size
-        for(i in len-30..len) {
-            values1.add(Entry(i.toFloat(), prices[i - 1]))
-        }
-
-        val dataset1 = LineDataSet(values1, "realtime")
-        dataset1.setDrawCircles(false)
-        dataset1.color = Color.rgb(255, 0, 0)
-
-//        val dataset2 = LineDataSet(values2, "realtime")
-//        dataset2.setDrawCircles(false)
-//        dataset2.color = Color.rgb(0, 0, 255)
-
-        data.add(dataset1)
-//        data.add(dataset2)
 
         chart.setDrawGridBackground(false)
         chart.description.isEnabled = false
@@ -94,19 +74,95 @@ class CropPredictedFragment : Fragment() {
         chart.xAxis.setDrawGridLines(false)
         chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
         chart.xAxis.granularity = 1f
-        chart.xAxis.valueFormatter = IndexAxisValueFormatter(dates)
-        // enable scaling and dragging
         chart.isDragEnabled = true
         chart.setScaleEnabled(true)
+        chart.isDoubleTapToZoomEnabled = false
 
         // if disabled, scaling can be done on x- and y-axis separately
         chart.setPinchZoom(false)
 
-        chart.data = LineData(data)
-        chart.invalidate()
+        viewModel.data.observe(viewLifecycleOwner, {
+            dates.addAll(it.keys)
+            prices.addAll(it.values)
 
+            val len = dates.size
+            for(i in 1..len) {
+                values1.add(Entry(i.toFloat(), prices[i - 1]))
+            }
+
+            val dataset1 = LineDataSet(values1, "realtime")
+            dataset1.setDrawCircles(false)
+            dataset1.color = Color.rgb(255, 0, 0)
+
+//        val dataset2 = LineDataSet(values2, "realtime")
+//        dataset2.setDrawCircles(false)
+//        dataset2.color = Color.rgb(0, 0, 255)
+
+            data.add(dataset1)
+//        data.add(dataset2)
+
+
+            chart.xAxis.valueFormatter = IndexAxisValueFormatter(dates)
+            // enable scaling and dragging
+
+
+            chart.data = LineData(data)
+            chart.onChartGestureListener = this
+
+            chart.invalidate()
+
+        })
 
         return view
     }
 
+    override fun onChartGestureStart(
+        me: MotionEvent?,
+        lastPerformedGesture: ChartTouchListener.ChartGesture?
+    ) {
+        Log.v("LongPress", "Start")
+    }
+
+    override fun onChartGestureEnd(
+        me: MotionEvent?,
+        lastPerformedGesture: ChartTouchListener.ChartGesture?
+    ) {
+        Log.v("LongPress", "End")
+    }
+
+    override fun onChartLongPressed(me: MotionEvent?) {
+        //pass
+    }
+
+    override fun onChartDoubleTapped(me: MotionEvent?) {
+        //why you always
+        Log.v("LongPress", "Double tap")
+        val x = chart.getHighlightByTouchPoint(me!!.x, me.y).x.toInt()
+        if(x < dates.size) {
+            val y = chart.getHighlightByTouchPoint(me.x, me.y).y
+            val date = dates[x]
+            Toast.makeText(activity, "$date $y", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onChartSingleTapped(me: MotionEvent?) {
+        //in the mood
+    }
+
+    override fun onChartFling(
+        me1: MotionEvent?,
+        me2: MotionEvent?,
+        velocityX: Float,
+        velocityY: Float
+    ) {
+        //flippin out
+    }
+
+    override fun onChartScale(me: MotionEvent?, scaleX: Float, scaleY: Float) {
+        //actin brand new
+    }
+
+    override fun onChartTranslate(me: MotionEvent?, dX: Float, dY: Float) {
+        //i aint tryna tell you what to do
+    }
 }
--- android app/app/src/main/java/com/example/farmerscollective/CropPredictedViewModel.kt
+++ android app/app/src/main/java/com/example/farmerscollective/CropPredictedViewModel.kt
@@ -1,7 +1,42 @@
 package com.example.farmerscollective
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+
+
+
 
 class CropPredictedViewModel : ViewModel() {
+    // Access a Cloud Firestore instance from your Activity
+    var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val _data = MutableLiveData<MutableMap<String, Float>>(mutableMapOf())
+
+    val data: LiveData<MutableMap<String, Float>>
+    get() = _data
+
+    init {
+        getData()
+    }
+
+    fun getData() {
+        db.collection("KOTA_Prices")
+            .get()
+            .addOnSuccessListener {
+                for(doc in it) {
+                    val prices = doc.data["data"] as ArrayList<HashMap<String, Any>>
+                    val temp = _data.value!!
+                    for(row in prices) {
+                        temp[row["DATE"]!!.toString()] = row["PRICE"]!!.toString().toFloat()
+                    }
+                    _data.value = temp
+                }
+            }
+            .addOnFailureListener {
+                Log.v("ViewModel", it.toString())
+            }
+    }
 
 }
